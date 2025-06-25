b5211213159b54f50dc39f0d704df0dc144b4c98
chenmingze22 <chenmingze22@gmail.com>
2025-06-25 16:47:45 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // ⚠️ 不要加 runtime: 'edge'
+  // ⚠ Do not add runtime: 'edge'
 }
 
 module.exports = nextConfig
